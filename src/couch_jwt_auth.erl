b16026d992483c093c4779c4af18d90cc5fa8454
lpuddu-interfacema <l.puddu@interfacema.de>
2017-06-27 16:33:42 +0200

    FIX: Wasn't using Result variable for LOG_ERROR

--- src/couch_jwt_auth.erl
+++ src/couch_jwt_auth.erl
@@ -66,7 +66,7 @@ jwt_authentication_handler(Req) ->
         Type:Msg ->
           Arr = #{"Exception" => Type, "Message" => Msg, "Stacktrace" => erlang:get_stacktrace()},
           Result = io:format("~90000p~n" , [Arr]),
-          ?LOG_ERROR("An exception occured while authenticating the token:  ~s", [Arr]),
+          ?LOG_ERROR("An exception occured while authenticating the token:  ~s", Result),
           throw({unauthorized, <<"Token rejected">>})
       end;
     _ -> Req
